9a7b8cfeb27130e712e95a30c85d4d39c2ecae0e
Steve Thompson <writetome51@gmail.com>
2018-09-27 21:09:25 -0600

    minor change

--- removeAdjacentToValue.js
+++ removeAdjacentToValue.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var getAndRemoveAdjacentToValue_1 = require("@writetome51/array-get-and-removers-intermediate/getAndRemoveAdjacentToValue");
+var getAndRemoveAdjacentToValue_1 = require("@writetome51/array-get-and-remove-adjacent-to-value/getAndRemoveAdjacentToValue");
 // info = {value: anyExceptObject, howMany: number, offset: number}
 function removeAdjacentToValue(info, array) {
     getAndRemoveAdjacentToValue_1.getAndRemoveAdjacentToValue(info, array);
